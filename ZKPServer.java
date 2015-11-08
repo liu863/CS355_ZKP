d32549c2ed804c6b4dc7e3af448b3d07b4a6d726
fang102 <fang102@purdue.edu>
2015-11-08 15:28:44 -0500

    Server modified

--- ZKPServer.java
+++ ZKPServer.java
@@ -1,5 +1,26 @@
 import java.util.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class ZKPServer {
+	public static void main(String[] args) throws IOException {
+        int portNumber = 2334;
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+		Socket clientSocket = serverSocket.accept();
+		System.out.println("Accepted");
+		try {
+		    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+		    BufferedReader in = new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
 
+		    out.println("Hi, there");
+		}
+		finally {
+			serverSocket.close();
+		}
+	}
 }
