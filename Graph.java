5dbf2e1d6944ff54d73ec64e01f3c039e976a794
mylzsd <liu863@purdue.edu>
2015-11-11 03:46:57 -0500

    Graph commitment isomorphism constructor

--- Graph.java
+++ Graph.java
@@ -16,7 +16,16 @@ public class Graph {
     
     //str = n|xx..x
     public Graph(String str) {
-        
+        int pipe = str.indexOf('|');
+        vnum = Integer.parseInt(str.substring(0, pipe));
+        pipe++;
+        adjmatrix = new int[vnum][vnum];
+        for (int i = 0; i < vnum; i++) {
+            for (int j = 0; j < vnum; j++) {
+                char val = str.charAt(pipe++);
+                adjmatrix[i][j] = Character.getNumericValue(val);
+            }
+        }
     }
     
     public boolean link(int x, int y) {
@@ -42,21 +51,33 @@ public class Graph {
     
     public Graph isomorphism(int[] p) {
         int[][] iso = new int[vnum][vnum];
-        
-        
+        for (int i = 0; i < vnum; i++) {
+            for (int j = 0; j < vnum; j++) {
+                iso[p[i]][p[j]] = adjmatrix[i][j];
+            }
+        }
         Graph g = new Graph(iso, vnum);
         return g;
     }
     
     public Graph commitment(int ran) {
-        return this;
+        int[][] com = new int[vnum][vnum];
+        for (int i = 0; i < vnum; i++) {
+            for (int j = 0; j < vnum; j++) {
+                com[i][j] = ((adjmatrix[i][j] + i + j) ^ ran) % 10;
+            }
+        }
+        Graph commit = new Graph(com, vnum);
+        return commit;
     }
     
-    /*
+    
     public static void main(String[] ris) {
-        int[][] adj = {{0, 1, 0}, {1, 0, 1}, {0, 1, 0}};
-        Graph g = new Graph(adj, 3);
+        //int[][] adj = {{0, 1, 0}, {1, 0, 1}, {0, 1, 0}};
+        Graph g = new Graph("4|0111101011011010");
         System.out.println(g.tostring());
+        Graph gc = g.commitment(365792);
+        System.out.println(gc.tostring());
     }
-    */
+    
 }
