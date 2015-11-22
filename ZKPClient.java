c02eee6789057200ce90e115fc3f16a52972aba2
fang102 <fang102@purdue.edu>
2015-11-22 01:54:52 -0500

    Almost Completed

--- ZKPClient.java
+++ ZKPClient.java
@@ -10,20 +10,20 @@ public class ZKPClient {
     private Socket s;
     private int sflag;
     
-    public int vnum;
+    public static int vnum;
     
-    private Graph g1;   //iso to g2p
-    private Graph g2;   //larger graph
-    private Graph g2p;  //subgraph of g2, iso to g1
+    private static Graph g1;   //iso to g2p
+    private static Graph g2;   //larger graph
+    private static Graph g2p;  //subgraph of g2, iso to g1
     
-    private int[] subgraph;        //vertices in subgraph g2p
-    private int[] isomorphism;     //isomorphic permutation from g2p to g2
+    private static int[] subgraph;        //vertices in subgraph g2p
+    private static int[] isomorphism;     //isomorphic permutation from g2p to g2
     
-    private int[][] rand_matrix;  //random number matrix to commit graph
-    private int[][] sub_rand_matrix;
+    private static int[][] rand_matrix;  //random number matrix to commit graph
+    private static int[][] sub_rand_matrix;
     
-    private HashMap<String, Integer> iso_perm;    //random permutation to generate Graph Q
-    private Integer iteration;
+    private static HashMap<String, Integer> iso_perm;    //random permutation to generate Graph Q
+    private static Integer iteration;
     
     public ZKPClient() {
         iteration = 0;
@@ -59,21 +59,65 @@ public class ZKPClient {
         }
         
         /* establish connection with server */
-        /*
+        int round = 0;
+
+        /* Initialize random matrix and flags */
+        rand_matrix = gen_randmatrix(vnum);
+        sub_rand_matrix = gen_sub_randmatrix(rand_matrix, g2); 
+
         ZKPClient client = new ZKPClient(args[0]);
-        client.readgraph(args[1]);
-        String ret = client.send ("123");
-        System.out.println("Client received : " + ret );
-        ret = client.send ("789");
-        System.out.println("Client received : " + ret );
-        ret = client.send ("exit");
-        System.out.println("Client received : " + ret );
-        ret = client.send ("789");
-        System.out.println("Client received : " + ret );
-        */
+
+        while ( round < 100 ) {
+            /* establish connection with server */
+            int[] qiso = rand_permutation(vnum); //pi
+            int[] alpha = genAlpha(qiso, isomorphism);
+
+            Graph Q = g2.isomorphism(qiso);
+            Graph Qp = g2p.isomorphism(qiso);
+
+            Graph HQ = Q.commitment(rand_matrix);
+            Graph HQp = Qp.commitment(sub_rand_matrix);
+
+            // send G1, G2
+            String ret = client.send("g1g2|" + g1.toString() + "|" + g2.toString());
+            System.out.println("clintmsg>> " + ret );
+
+            // send HQ
+            ret = client.send("hq|" + HQ.toString());
+            System.out.println("clintmsg>> " + ret );
+
+            // check server request and respond
+            String[] str = ret.split("\\|");
+            int req = Integer.parseInt(str[1]);
+
+            if ( req == 0 ) {
+                // send Q, pi, rand_matrix
+                String s = arraytoString(qiso, vnum);
+                ret = client.send("prf|" + Q.toString() + "|" +  s + "|" + darraytoString(rand_matrix, vnum) );
+
+            }
+            else {
+                // send Qp, alpha
+                String s = arraytoString(alpha, vnum);
+                ret = client.send("prf|" + Qp.toString() + "|" +  s + "|" + darraytoString(sub_rand_matrix, vnum) );
+            }
+            System.out.println("clintmsg>>  " + ret );
+            if ( ret.equals("true") ) {
+                round ++;
+                continue;
+            }
+            else {
+                System.out.println("ZKP verification failed!");
+                ret = client.send("exit");
+                System.out.println("clintmsg>>  " + ret );
+            }
+        }
+        System.out.println("ZKP verification Succeed!");
+        String ret = client.send("exit");
+        System.out.println("clintmsg>>  " + ret );
     }
 
-    private int[] rand_permutation(int vnum){
+    private static int[] rand_permutation(int vnum){
         int[] permutation = new int[vnum];
         //ascending permutation
         for(int i = 0; i < vnum; i++){
@@ -125,18 +169,8 @@ public class ZKPClient {
         }
         return line;
     }
-    
-    private static int[][] gen_randmatrix( int vnum ) {
-        int[][] randm = new int[vnum][vnum];
-        for (int i = 0; i < vnum; i++) {
-            for (int j = 0; j < vnum; j++) {
-                randm[i][j] = 10000 + (int)( Math.random() * 90000  );
-            }
-        } 
-        return randm;
-    }
-    
-    private static int[][] gen_sub_randmatrix( int[][] randm, Graph subgraph ) {
+
+    public static int[][] gen_sub_randmatrix( int[][] randm, Graph subgraph ) {
         int vnum = subgraph.getvnum();
         int[][] sub_randm = new int[vnum][vnum];
         for (int i = 0; i < vnum; i++) {
@@ -150,6 +184,54 @@ public class ZKPClient {
         } 
         return sub_randm;
     }
+
+    private static String arraytoString(int[] arr, int vnum) {
+        StringBuilder sb = new StringBuilder(vnum);
+            for (int i : arr) {
+              sb.append(i);
+              sb.append(",");
+            }
+        String s = sb.toString();
+        return s;
+    }
+
+    private static String darraytoString(int[][] darr, int vnum) {
+        String str = new String();
+        for (int i = 0; i < vnum; i++) {
+            for (int j = 0; j < vnum; j++) {
+                if ( darr[i][j] != 0 )
+                    str = str.concat(Integer.toString(darr[i][j]));
+                else {
+                    str = str.concat("00000");
+                }
+            }
+        }
+        return str;
+    }
+
+    private static int[] genAlpha(int[] Pi, int[] Iso) {
+        int[] tmp = new int[vnum];
+        int[] alpha = new int[vnum];
+
+        for ( int i = 0; i < vnum; i++ )
+            tmp[Pi[i]] = i;
+
+        for ( int i = 0; i < vnum; i++ )
+            alpha[i] = Iso[tmp[i]];
+
+        return alpha;
+    }
+    
+    private static int[][] gen_randmatrix( int vnum ) {
+        int[][] randm = new int[vnum][vnum];
+        for (int i = 0; i < vnum; i++) {
+            for (int j = 0; j < vnum; j++) {
+                randm[i][j] = 10000 + (int)( Math.random() * 90000  );
+            }
+        } 
+        return randm;
+    }
+
     
     private void readgraph(String file) {
         try {
--- ZKPServer.java
+++ ZKPServer.java
@@ -9,25 +9,26 @@ import java.io.BufferedReader;
 
 public class ZKPServer {
 
-    private static int round;
-    private Graph HQ;
-    private int HR;
-    private Graph G1;
-    private Graph G2;
-    private static boolean[] res;
-    private Graph Q;
-    private Graph Qp;
-    private int[] perm;
-    private int[][] rand_matrix;
-    private int[][] sub_rand_matrix;
+    private static Graph HQ;
+    private static Graph HQp;
+    private static Graph G1;
+    private static Graph G2;
+    private static Graph Q;
+    private static Graph Qp;
+    private static int[] perm;
+    private static int[][] rand_matrix;
+    private static int[][] sub_rand_matrix;
 
     public static void main(String[] args) throws IOException {
         
         /* Initilization */
-        round = 0;
+        int req = 0;
+        String vnumstr = null;
+        int vnum = 0;
+        int round = 0;
         int portNumber = 2334;
         ServerSocket serverSocket = new ServerSocket(portNumber);
-        res = new boolean[100];
+        boolean[] res = new boolean[100];
         
         while (true) {
             Socket clientSocket = serverSocket.accept();
@@ -39,12 +40,48 @@ public class ZKPServer {
                 while ( true ) {
                     if ( ( inputLine = in.readLine() ) == null )
                         break;
-                    System.out.println("Handling Request: " + inputLine );
-                    if ( inputLine.equals("123") ) {
-                        out.println("456");
+                    System.out.println("reqmsg>> " + inputLine );
+                    if ( inputLine.contains("g1g2") ) {
+                        String[] str = inputLine.split("\\|");
+                        vnumstr = str[1];
+                        vnum = Integer.parseInt(str[1]);
+                        String str2 = new String(str[1] + "|" + str[2]);
+                        String str4 = new String(str[1] + "|" + str[4]);
+                        G1 = new Graph(str2);
+                        G2 = new Graph(str4);
+                        out.println("eg1g2");
                     }
-                    if ( inputLine.equals("789") ) {
-                        out.println("101112");
+                    if ( inputLine.contains("hq") ) {
+                        String[] str = inputLine.split("\\|");
+                        String str1 = new String(vnumstr + "|" + str[2]);
+                        HQ = new Graph(str1);
+                        if ( Math.random() < 0.5) {
+                            out.println("req|0");
+                        }
+                        else {
+                            out.println("req|1");
+                            req = 1;
+                        }
+                    }
+                    if ( inputLine.contains("prf") ) {
+                        String[] str = inputLine.split("\\|");
+                        if ( req == 0 ) {
+                            String Qs = new String(vnumstr + "|" + str[2]);
+                            Q = new Graph(Qs);
+                            perm = stringtoArray(str[3], vnum);
+                            rand_matrix = str2arry(str[4]);
+                            HQ = Q.commitment(rand_matrix);
+                            out.println(execute(0));
+                        }
+                        else {
+                            String Qps = new String(vnumstr + "|" + str[2]);
+                            Qp = new Graph(Qps);
+                            perm = stringtoArray(str[3], vnum);
+                            sub_rand_matrix = str2arry(str[4]);
+                            HQp = Qp.commitment(sub_rand_matrix);
+                            out.println(execute(1));
+                        }
+                        req = 0;
                     }
                     if ( inputLine.equals("exit") ) {
                         out.println("exit");
@@ -60,26 +97,53 @@ public class ZKPServer {
     }
 
     /* Verify the proof */
-    public boolean execute(int mode) {
+    private static boolean execute(int mode) {
         boolean ret = false;
         if (mode == 0) {
             Graph myHQ = Q.commitment(rand_matrix);
+            for (int i = 0; i < myHQ.getvnum(); i++) {
+                for (int j = 0; j < myHQ.getvnum(); j++) {
+                    if ( ( Q.getv(i,j) == 1 ) && ( myHQ.getv(i,j) != HQ.getv(i,j) ) )
+                        return false;
+                }
+            }
             Graph PiG2 = G2.isomorphism(perm);
-            ret = myHQ.equals(HQ) && Q.equals(PiG2);
+            ret = Q.equals(PiG2);
         }
         else {
             Graph myHQp = Qp.commitment(sub_rand_matrix);
             for (int i = 0; i < myHQp.getvnum(); i++) {
                 for (int j = 0; j < myHQp.getvnum(); j++) {
-                    if ( ( Qp.getv(i,j) == 1 ) && ( myHQp.getv(i,j) != HQ.getv(i,j) ) )
+                    if ( ( Qp.getv(i,j) == 1 ) && ( myHQp.getv(i,j) != HQp.getv(i,j) ) )
                         return false;
                 }
             }
-            Graph PiG1 = G1.isomorphism(perm);
-            ret = Qp.equals(PiG1);
+            Graph alphaQp = Qp.isomorphism(perm);
+            ret = G1.equals(alphaQp);
             
         }
         return ret;
+    }
+
+    public static int[][] str2arry(String str) {
+        int num = (int)Math.sqrt(str.length() / 5);
+        int[][] ret = new int[num][num];
+        for (int i = 0; i < str.length();) {
+            String temp = str.substring(i, i + 5);
+            int t = Integer.parseInt(temp);
+            ret[(i / 5) / num][(i / 5) % num] = t;
+            i = i + 5;
+        }
+        return ret;
+    }
+
+    private static int[] stringtoArray(String s, int vnum) {
+        String str[] = s.split(",");
+        int[] tmp = new int[vnum];
+        for(int i = 0; i < vnum; i++){
+            tmp[i] = Integer.parseInt(str[i]) ;
+        }
+        return tmp;
     }
  
 }
